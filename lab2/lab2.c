7bc7e5d5de2e0bd3c4fdb218deb9ea5ac39d32cb
up201405381 <up201405381@c565d994-3a90-4353-ac06-00fa9734bba2>
2015-10-06 09:12:21 +0000

    Missing includes in timer.c.

--- lab2/lab2.c
+++ lab2/lab2.c
@@ -3,6 +3,9 @@
 
 int main(int argc, char **argv) {
 
+	timer_test_config(0);
+	timer_test_config(1);
+	timer_test_config(2);
 
 	return 0;
 }
--- lab2/timer.c
+++ lab2/timer.c
@@ -1,6 +1,6 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
-#include "i8254"
+#include "i8254.h"
 #include "timer.h"
 
 int timer_set_square(unsigned long timer, unsigned long freq) {
