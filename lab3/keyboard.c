3038c2009daf33e8ad0745c006f37dca9442b77c
up201404131 <up201404131@c565d994-3a90-4353-ac06-00fa9734bba2>
2015-10-26 16:39:00 +0000

    Almost finished test_leds function

--- lab3/keyboard.c
+++ lab3/keyboard.c
@@ -1,53 +1,55 @@
 #include <minix/syslib.h>
 #include <minix/drivers.h>
 #include "keyboard.h"
+#include "timer.h"
 
 int hook_id;
 
-int kb_subscribe_int(void){
+int kb_subscribe_int(void) {
 
-	hook_id=4;
+	hook_id = 4;
 	int temp = hook_id;
-	if(sys_irqsetpolicy(KB_IRQ,IRQ_REENABLE|IRQ_EXCLUSIVE,&hook_id)!=0)
+	if (sys_irqsetpolicy(KB_IRQ, IRQ_REENABLE | IRQ_EXCLUSIVE, &hook_id) != 0)
 		return -1;
 
-	if(sys_irqenable(&hook_id)!=0)
+	if (sys_irqenable(&hook_id) != 0)
 		return -1;
 
 	return temp;
 }
 
-int kb_unsubscribe_int(void){
+int kb_unsubscribe_int(void) {
 
-	if(sys_irqrmpolicy(&hook_id)!=0)
+	if (sys_irqrmpolicy(&hook_id) != 0)
 		return -1;
 
-	if(sys_irqdisable (&hook_id)!=0)
+	if (sys_irqdisable(&hook_id) != 0)
 		return -1;
 
 	return 0;
 }
 
-long kb_int_handler(void){
+long kb_int_handler(void) {
 
-	unsigned long stat,word;
+	unsigned long stat, word;
 	static unsigned long last = 0x00;
 	int attempts = 5;
 	int i = 0;
 
-	while(i < attempts){
+	while (i < attempts) {
 		i++;
-		sys_inb(KB_STATUS,&stat);
-		if((stat & KB_OUTBUF_FULL) && !(stat & (KB_STAT_PARITY | KB_STAT_TIMEOUT))){ //checks if the output buffer is full and there are no errors
+		sys_inb(KB_STATUS, &stat);
+		if ((stat & KB_OUTBUF_FULL)
+				&& !(stat & (KB_STAT_PARITY | KB_STAT_TIMEOUT))) { //checks if the output buffer is full and there are no errors
 			sys_inb(KB_OUT_BUF, &word); //loads word var with the content of the buffer
-			if(word == KB_2BYTE_SCODE){
-				last=word;
+			if (word == KB_2BYTE_SCODE) {
+				last = word;
 				return word; //reads the 0xE0 code, which signals a 2byte scancode, and stores it in the static last
 			}
 
-			if(last == KB_2BYTE_SCODE){
-				word=(last<<4 | word); // completes the 2 byte scancode
-				last=0x00;
+			if (last == KB_2BYTE_SCODE) {
+				word = (last << 4 | word); // completes the 2 byte scancode
+				last = 0x00;
 				return word;
 			}
 
@@ -56,23 +58,66 @@ long kb_int_handler(void){
 		tickdelay(micros_to_ticks(DELAY_US));
 	}
 
-
 }
 
-void kb_toggle_led(unsigned int led){
-	unsigned long stat;
-	int i=0, attempts=5;
+unsigned short capslock = 0;
+unsigned short numlock = 0;
+unsigned short scrlock = 0;
 
-	sys_outb(KB_OUT_BUF, KB_LED_CMD);
+void kb_toggle_led(unsigned int led) {
+	unsigned long stat, msg;
+	int i = 0, attempts = 5;
+	unsigned short cmd = 0;
 
-	while(i < attempts){
-		i++;
-		sys_inb(KB_OUT_BUF,&stat);
-		if(!(stat & KB_OUTBUF_FULL)){
-			sys_outb(KB_OUT_BUF, led);
+
+
+
+
+	switch (led) {
+	case (0):{
+		scrlock = abs(scrlock-1);
+		break;
+	}
+
+	case (1): {
+		numlock = abs(numlock-1);
+		break;
+	}
+
+	case (2): {
+		capslock = abs(capslock-1);
+		break;
+	}
+
+	default: {
+		printf("Invalid input\n");
+		return;
+	}
+	}
+
+	cmd = (capslock << 2) | (numlock << 1) | scrlock;
+
+	do {
+		sys_outb(KB_IN_BUF, KB_LED_CMD);
+		tickdelay(micros_to_ticks(DELAY_US));
+		sys_inb(KB_OUT_BUF, &msg);
+	} while (msg != ACK);
+
+	do {
+
+		sys_outb(KB_OUT_BUF, cmd);
+		tickdelay(micros_to_ticks(DELAY_US));
+		sys_inb(KB_OUT_BUF, &msg);
+		if(msg == ACK)
+			break;
+
+		if(msg == ERROR){
+			kb_toggle_led(led);
 			break;
 		}
-		tickdelay(micros_to_ticks(DELAY_US));
-	}
+
+	} while (msg == RESEND);
+
+
 }
 
--- lab3/keyboard.h
+++ lab3/keyboard.h
@@ -7,12 +7,16 @@
 #define KB_STATUS 0x64
 #define KBC_COMMAND 0x64
 #define KB_OUT_BUF 0x60
+#define KB_IN_BUF 0x60
 #define KB_INBUF_FULL BIT(1)
 #define KB_OUTBUF_FULL BIT(0)
 #define KB_STAT_PARITY BIT(7)
 #define KB_STAT_TIMEOUT BIT(6)
 #define KB_2BYTE_SCODE 0xE0
 #define KB_LED_CMD 0xED
+#define ACK 0xFA
+#define RESEND 0xFE
+#define ERROR 0xFC
 
 /**
  * @brief Subscribes and enables keyboard interrupts
