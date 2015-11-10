475c4365e31ddd32ee331bc9ca3dbd8c20f92f73
up201404131 <up201404131@c565d994-3a90-4353-ac06-00fa9734bba2>
2015-11-10 08:27:54 +0000

    Fixed accidentaly copying same things twice on timer.c

---
+++ lab4/timer.h
@@ -0,0 +1,116 @@
+#ifndef __TIMER_H
+#define __TIMER_H
+
+/** @defgroup timer timer
+ * @{
+ *
+ * Functions for using the i8254 timers
+ */
+
+/**
+ * @brief Configures a timer to generate a square wave
+ * 
+ * Does not change the LSB (BCD/binary) of the timer's control word.
+ * 
+ * @param timer Timer to configure. (Ranges from 0 to 2)
+ * @param freq Frequency of the square wave to generate
+ * @return Return 0 upon success and non-zero otherwise
+ */
+int timer_set_square(unsigned long timer, unsigned long freq);
+
+/**
+ * @brief Subscribes and enables Timer 0 interrupts
+ *
+ * @return Returns bit order in interrupt mask; negative value on failure
+ */
+int timer_subscribe_int(void );
+
+/**
+ * @brief Unsubscribes Timer 0 interrupts
+ *
+ * @return Return 0 upon success and non-zero otherwise
+ */
+int timer_unsubscribe_int();
+
+/**
+ * @brief Timer 0 interrupt handler
+ *
+ * Increments counter
+ */
+void timer_int_handler();
+
+/**
+ * @brief Reads the input timer configuration via read-back command
+ *
+ * @param timer Timer whose config to read (Ranges from 0 to 2)
+ * @param st    Address of memory position to be filled with the timer config
+ * @return Return 0 upon success and non-zero otherwise
+ */
+int timer_get_conf(unsigned long timer, unsigned char *st);
+
+/**
+ * @brief Shows timer configuration
+ * 
+ * Displays in a human friendly way, the configuration of a timer
+ *  as read via the read-back command, by providing the values 
+ *  (and meanings) of the different components of a timer configuration 
+ *
+ * @param conf configuration to display in human friendly way
+ * @return Return 0 upon success and non-zero otherwise
+ */
+int timer_display_conf(unsigned char conf);
+
+
+
+/**
+ * @brief Tests programming timer in square wave mode
+ *
+ * Programs Timer 0 to generate square mode with input frequency
+ *
+ * @param freq Frequency of square wave to generate
+ * @return Return 0 upon success and non-zero otherwise
+ */
+int timer_test_square(unsigned long freq);
+
+/**
+ * @brief Tests Timer 0 interrupt handling
+ *
+ * Subscribes Timer 0 interrupts and prints a message once
+ *  per second for the specified time interval
+ *
+ * @param time Length of time interval while interrupts are subscribed
+ * @return Return 0 upon success and non-zero otherwise
+ */
+int timer_test_int(unsigned long time);
+
+/**
+ * @brief Tests display of timer config
+ *
+ * Just calls timer_get_conf() followed by timer_display_conf()
+ *
+ * @param timer Timer whose config to read (Ranges from 0 to 2)
+ * @return Return 0 upon success and non-zero otherwise
+ */
+int timer_test_config(unsigned long timer);
+
+
+/**
+ * @brief Timer 0 interrupt handler with added keyboard led toggling
+ *
+ * if a second passes (counter % 60 == 0) it toggles the led passed in the parameter led
+ *
+ * @param j, led array counter, is incremented when the above condition is verified
+ * @param led, led to be toggled on/off (0 scroll lock/ 1 num lock/ 2 caps lock)
+ */
+void timer_int_handler_leds(unsigned short *j,unsigned short led);
+
+/**
+ * @brief Timer 0 interrupt handler that increments a counter
+ *
+ * if a second passes (counter % 60 = 0) it increments the value pointed by time
+ *
+ * @param time, the value (passed by reference) of the seconds since timed_test_scan was called
+ */
+void timed_scan_int_handler(unsigned short *time);
+
+#endif /* __TIMER_H */
