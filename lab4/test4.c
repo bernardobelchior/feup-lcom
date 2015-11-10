47718e7def4f5ce0394f8b753cdd99a9782251a2
up201404131 <up201404131@c565d994-3a90-4353-ac06-00fa9734bba2>
2015-11-10 08:49:06 +0000

    Added some files

--- lab4/test4.c
+++ lab4/test4.c
@@ -8,11 +8,16 @@
 unsigned int count = 0;
 
 int test_packet(unsigned short cnt) {
-	int irq_set = mouse_subscribe_int();
+	int irq_set_mouse = mouse_subscribe_int();
 	int r;
 	int ipc_status;
 	char packet[3];
 	message msg;
+
+	if (irq_set_mouse >= 0)
+		irq_set_mouse = BIT(irq_set_mouse);
+	else
+		irq_set_mouse = 0;
 
 	unsigned int i;
 	for (i = 0; i < cnt; i++) {
@@ -24,20 +29,21 @@ int test_packet(unsigned short cnt) {
 		if (is_ipc_notify(ipc_status)) { /* received notification */
 			switch (_ENDPOINT_P(msg.m_source)) {
 			case HARDWARE: /* hardware interrupt notification */
-				//if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
-				count++;
-				mouse_int_handler(count, packet);
-				printf("Byte %d: %c", count, packet[count - 1]);
-				if (count == 3) {
-					printf("\n");
-					count = 0;
-				}
-				break;
+				if (msg.NOTIFY_ARG & irq_set_mouse) { /* subscribed interrupt */
+					count++;
+					mouse_int_handler(count, packet);
+					printf("Byte %d: %c", count, packet[count - 1]);
+					if (count == 3) {
+						printf("\n");
+						count = 0;
+					}
+					break;
 			default:
 				break; /* no other notifications expected: do nothing */
+				} else { /* received a standard message, not a notification */
+					/* no standard messages expected: do nothing */
+				}
 			}
-		} else { /* received a standard message, not a notification */
-			/* no standard messages expected: do nothing */
 		}
 	}
 }
@@ -47,10 +53,15 @@ int test_async(unsigned short idle_time) {
 	int irq_set_timer = timer_subscribe_int();
 	int r;
 	int ipc_status;
-	unsigned short time=0;
+	unsigned short time = 0;
 
 	char packet[3];
 	message msg;
+
+	if (irq_set_mouse >= 0)
+		irq_set_mouse = BIT(irq_set_mouse);
+	else
+		irq_set_mouse = 0;
 
 	while (time < idle_time) {
 		/* Get a request message. */
@@ -71,7 +82,7 @@ int test_async(unsigned short idle_time) {
 						count = 0;
 					}
 				}
-				if (msg.NOTIFY_ARG & irq_set_timer){
+				if (msg.NOTIFY_ARG & irq_set_timer) {
 					timed_scan_int_handler(&time);
 				}
 
@@ -89,18 +100,17 @@ int test_config(void) {
 	char packet[3];
 	char ack;
 	int error = 0;
-	while(error){
+	while (error) {
 		error = 0;
 		write_to_KBC(KBC_COMMAND, WRITE_TO_MOUSE);
 		write_to_KBC(KBC_OUT_BUF, MOUSE_STATUS_REQUEST);
 		unsigned int i;
-		for(i = 0; i < 3; i++){
+		for (i = 0; i < 3; i++) {
 			read_from_KBC(KBC_IN_BUF, &ack);
-			if(ack != ACK){
+			if (ack != ACK) {
 				error = 1;
 				break;
-			}
-			else{
+			} else {
 				read_from_KBC(KBC_OUT_BUF, &packet[i]);
 				printf("Byte %d: %x\t", i, packet[i]);
 			}
