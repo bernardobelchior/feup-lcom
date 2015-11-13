bafb9dc5649d1dd3f97c703bae5d56ec74dffc0c
up201405381 <up201405381@c565d994-3a90-4353-ac06-00fa9734bba2>
2015-11-13 18:10:44 +0000

    First commit of lab5. Not sure how to parse argument xmp of test_xmp and test_move

---
+++ lab5/lab5.c
@@ -0,0 +1,247 @@
+#include "test5.h"
+#include "pixmap.h"
+#include <minix/drivers.h>
+
+static int proc_args(int argc, char *argv[]);
+static unsigned short parse_ushort(char *str, int base);
+static long parse_long(char *str, int base);
+static unsigned long parse_ulong(char *str, int base);
+static void print_usage(char *argv[]);
+static long parse_short(char *str, int base);
+
+int main(int argc, char **argv) {
+	sef_startup();
+	proc_args(argc, argv);
+	return 0;
+}
+
+static void print_usage(char *argv[]) {
+	printf(
+			"Usage: one of the following:\n"
+			"\t service run %s -args \"test_init <unsigned short mode> <unsigned short delay>\" \n"
+			"\t service run %s -args \"test_square <unsigned short x> <unsigned short y> <unsigned short size> <unsigned long color>\" \n"
+			"\t service run %s -args \"test_line <unsigned short xi> <unsigned short yi> <unsigned short xf> <unsigned short yf> <unsigned long color> \" \n"
+			"\t service run %s -args \"test_xpm <unsigned short xi> <unsigned short yi> <char array xpm>\" \n",
+			"\t service run %s -args \"test_move <unsigned short xi> <unsigned short yi> <char array xpm> <unsigned short hor> <short delta> <unsigned short time>\" \n",
+			"\t service run %s -args \"test_controller\" \n",
+			argv[0], argv[0], argv[0], argv[0], argv[0], argv[0]);
+}
+
+static int proc_args(int argc, char *argv[]) {
+
+	unsigned short mode, delay, x, y, size, xf, yf, hor, time;
+	short delta;
+	unsigned long color;
+	char[] *xpm;
+
+	/* check the function to test: if the first characters match, accept it */
+	if (argc == 1) {
+		print_usage(argv);
+		return 1;
+	}
+
+	else if (strncmp(argv[1], "test_init", strlen("test_init")) == 0) {
+		if (argc != 4) {
+			printf("VBE: wrong no of arguments for test_init() \n");
+			return 1;
+		}
+
+		if ((mode = parse_ushort(argv[2], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((delay = parse_ushort(argv[3], 10)) == USHRT_MAX)
+			return 1;
+
+		printf("VBE:: test_init(%d, %d)\n", mode, delay);
+		return test_init(mode, delay);
+	}
+
+	else if (strncmp(argv[1], "test_square", strlen("test_square")) == 0) {
+		if (argc != 6) {
+			printf("VBE: wrong no of arguments for test_square() \n");
+			return 1;
+		}
+
+		if ((x = parse_ushort(argv[2], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((y = parse_ushort(argv[3], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((size = parse_ushort(argv[4], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((color = parse_ulong(argv[5], 10)) == ULONG_MAX)
+			return 1;
+
+		printf("VBE::test_square(%d, %d, %d, %d)\n", x, y, size, color);
+		return test_square(x, y, size, color);
+	}
+
+	else if (strncmp(argv[1], "test_line", strlen("test_line")) == 0) {
+		if (argc != 7) {
+			printf("VBE: wrong no of arguments for test of test_line() \n");
+			return 1;
+		}
+
+		if ((x = parse_ushort(argv[2], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((y = parse_ushort(argv[3], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((xf = parse_ushort(argv[4], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((yf = parse_ushort(argv[5], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((color = parse_ulong(argv[6], 10)) == ULONG_MAX)
+			return 1;
+
+		printf("VBE::test_line(%d, %d, %d, %d, %d)\n", x, y, xf, yf, color);
+		return test_line(x, y, xf, yf, color);
+	}
+
+	else if (strncmp(argv[1], "test_xpm", strlen("test_xpm")) == 0) {
+		if (argc != 4) {
+			printf("VBE: wrong no of arguments for test of test_xpm() \n");
+			return 1;
+		}
+
+		if ((x = parse_ushort(argv[2], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((y = parse_ushort(argv[3], 10)) == USHRT_MAX)
+			return 1;
+
+		printf("VBE::test_xpm(%d,%d)\n", x, y, xpm);
+		return test_xpm(x, y, xpm);
+
+	}
+	else if (strncmp(argv[1], "test_move", strlen("test_move")) == 0) {
+		if (argc != 8) {
+			printf("VBE: wrong no of arguments for test of test_move() \n");
+			return 1;
+		}
+
+		if ((x = parse_ushort(argv[2], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((y = parse_ushort(argv[3], 10)) == USHRT_MAX)
+			return 1;
+
+		if ((hor = parse_ushort(argv[5], 10)) == USHRT_MAX)
+					return 1;
+
+		if ((delta = parse_short(argv[6], 10)) == SHRT_MAX)
+					return 1;
+
+		if ((time = parse_ushort(argv[7], 10)) == USHRT_MAX)
+					return 1;
+
+
+		printf("VBE::test_move(%d,%d)\n", length, tolerance);
+		return test_move(length, tolerance);
+
+	}
+	else if (strncmp(argv[1], "test_controller", strlen("test_controller")) == 0) {
+		if (argc != 2) {
+			printf("VBE: wrong no of arguments for test of test_controller() \n");
+			return 1;
+		}
+
+		printf("VBE::test_xpm()\n");
+		return test_controller();
+
+	}
+	else {
+		printf("VBE: non valid function \"%s\" to test\n", argv[1]);
+		return 1;
+	}
+}
+
+static unsigned short parse_ushort(char *str, int base) {
+	char *endptr;
+	unsigned long val;
+
+	val = (unsigned short) strtoul(str, &endptr, base);
+
+	if ((errno == ERANGE && val == USHRT_MAX) || (errno != 0 && val == 0)) {
+		perror("strtol");
+		return USHRT_MAX;
+	}
+
+	if (endptr == str) {
+		printf("VBE: parse_ushort: no digits were found in %s \n", str);
+		return USHRT_MAX;
+	}
+
+	/* Successful conversion */
+	return val;
+}
+
+static unsigned long parse_ulong(char *str, int base) {
+	char *endptr;
+	unsigned long val;
+
+	val = strtoul(str, &endptr, base);
+
+	if ((errno == ERANGE && (val == ULONG_MAX || val == ULONG_MIN))
+			|| (errno != 0 && val == 0)) {
+		perror("strtol");
+		return ULONG_MAX;
+	}
+
+	if (endptr == str) {
+		printf("VBE: parse_long: no digits were found in %s \n", str);
+		return ULONG_MAX;
+	}
+
+	/* Successful conversion */
+	return val;
+}
+
+static long parse_long(char *str, int base) {
+	char *endptr;
+	unsigned long val;
+
+	val = strtol(str, &endptr, base);
+
+	if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+			|| (errno != 0 && val == 0)) {
+		perror("strtol");
+		return LONG_MAX;
+	}
+
+	if (endptr == str) {
+		printf("VBE: parse_long: no digits were found in %s \n", str);
+		return LONG_MAX;
+	}
+
+	/* Successful conversion */
+	return val;
+}
+
+static long parse_short(char *str, int base) {
+	char *endptr;
+	unsigned long val;
+
+	val = strtol(str, &endptr, base);
+
+	if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN))
+			|| (errno != 0 && val == 0)
+			|| (errno == ERANGE && (val < SHRT_MIN || val > SHRT_MAX))) {
+		perror("strtol");
+		return SHRT_MAX;
+	}
+
+	if (endptr == str) {
+		printf("VBE: parse_long: no digits were found in %s \n", str);
+		return SHRT_MAX;
+	}
+
+	/* Successful conversion */
+	return val;
+}
+
---
+++ lab5/timer.c
@@ -0,0 +1,170 @@
+#include <minix/syslib.h>
+#include <minix/drivers.h>
+#include "i8254.h"
+#include "timer.h"
+
+unsigned int counter = 0;
+int hook_id;
+
+int timer_set_square(unsigned long timer, unsigned long freq) {
+
+	unsigned int n = TIMER_FREQ / freq;
+
+	char lsb = (char) n ;
+	char msb = (char) (n >> 8);
+
+	sys_outb(TIMER_CTRL, TIMER_SEL0 | TIMER_LSB_MSB | TIMER_SQR_WAVE | TIMER_BIN);
+	sys_outb(TIMER_0, lsb);
+	sys_outb(TIMER_0, msb);
+
+
+
+	return 1;
+}
+
+int timer_subscribe_int(void ) {
+
+
+	hook_id = 10;
+	int temp=hook_id;
+
+	if(sys_irqsetpolicy(TIMER0_IRQ, IRQ_REENABLE, &hook_id) != 0)
+		return -1;
+
+	if(sys_irqenable(&hook_id) != 0)
+		return -1;
+
+
+	return temp;
+	return -1;
+}
+
+int timer_unsubscribe_int() {
+
+	if(sys_irqrmpolicy(&hook_id) != 0)
+		return 1;
+
+	if(sys_irqdisable (&hook_id) !=0)
+		return 1;
+
+	return 0;
+	return 1;
+}
+
+void timer_int_handler() {
+
+	counter++;
+	if(counter % 60 == 0){
+		printf ("%d segundo(s)\n", counter/60);
+	}
+
+}
+
+int timer_get_conf(unsigned long timer, unsigned char *st) {
+	char read = TIMER_RB_CMD | TIMER_RB_SEL(timer);
+	char memory;
+	unsigned long conf;
+
+	switch(timer){
+		case 0:
+			memory = TIMER_0;
+			break;
+		case 1:
+			memory = TIMER_1;
+			break;
+		case 2:
+			memory = TIMER_2;
+			break;
+	}
+
+	sys_outb(TIMER_CTRL, read);
+	int ret = sys_outb(memory, &conf);
+	*st = (char) conf;
+	return ret;
+}
+
+int timer_display_conf(unsigned char conf) {
+	
+	printf("Output: %d\n", (conf & 0x80) >> 7 );
+	printf("Null Count: %d\n", (conf & 0x40) >> 6 );
+	printf("Type of Access: ");
+
+	if(conf & 0x30 == 0x30)
+			printf("LSB followed by MSB\n");
+		else if (conf & 0x20 == 0x20)
+			printf("MSB\n");
+		else
+			printf("LSB\n");
+
+	printf("Programmed Mode: " );
+
+	switch((conf & 0x0E) >> 1){
+	case 0x00: printf("Interrupt on Terminal Count");
+		break;
+	case 0x01: printf("Hardware Retriggerable One-Shot");
+		break;
+	case 0x02: printf("Rate Generator");
+		break;
+	case 0x03: printf("Square Wave Mode");
+		break;
+	case 0x04: printf("Software Triggered Strobe");
+			break;
+	case 0x05: printf("Hardware Triggered Strobe (Retriggerable)");
+			break;
+	}
+
+	printf("\n");
+	printf("BCD: %s", (conf & 0x01) ? "BCD\n" : "Binary\n");
+
+	return 0;
+}
+
+int timer_test_square(unsigned long freq) {
+	timer_set_square(0, freq);
+}
+
+int timer_test_int(unsigned long time) {
+
+	int ipc_status;
+	 message msg;
+	 int irq_set = timer_subscribe_int();
+	 int r;
+
+	 if(irq_set >= 0)
+		 irq_set = BIT(irq_set);
+	 else
+		 irq_set = 0;
+
+	 while( counter/60 < time ) { /* You may want to use a different condition */
+	    /* Get a request message. */
+	    if ( (r = driver_receive(ANY, &msg, &ipc_status)) != 0 ) {
+	        printf("driver_receive failed with: %d", r);
+	        continue;
+	    }
+	    if (is_ipc_notify(ipc_status)) { /* received notification */
+	        switch (_ENDPOINT_P(msg.m_source)) {
+	            case HARDWARE: /* hardware interrupt notification */
+	                if (msg.NOTIFY_ARG & irq_set) { /* subscribed interrupt */
+	                    timer_int_handler();
+	                    /* process it */
+	                }
+	                break;
+	            default:
+	                break; /* no other notifications expected: do nothing */
+	        }
+	    } else { /* received a standard message, not a notification */
+	        /* no standard messages expected: do nothing */
+	    }
+	 }
+
+
+	return 1;
+}
+
+int timer_test_config(unsigned long timer) {
+	unsigned char st;
+	timer_get_conf(timer, &st);
+	timer_display_conf(st);
+	
+	return 1;
+}
