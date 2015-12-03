e017f200b75ecabc67afa3e2f52ab89ddf0b5b58
up201405381 <up201405381@c565d994-3a90-4353-ac06-00fa9734bba2>
2015-12-03 14:57:53 +0000

    Added highscore

--- proj/highscore.c
+++ proj/highscore.c
@@ -8,44 +8,90 @@ int highscore_init(){
 	highscores = (score**) malloc(MAX_NUMBER_OF_SCORES*sizeof(score*));
 	score* sc;
 
+	fscanf(file, "//MINIX INVADERS HIGHSCORES//\n");
+
 	unsigned char i = 0;
+
 	while((sc = highscore_read(file)) != NULL){
 		highscores[i] = sc;
 		i++;
 	}
 
 	highscore_size = i;
-
+	fclose(file);
 	return 0;
 }
 
 score* highscore_read(FILE* file){
-	score* sc = (score*) malloc(sizeof(score));
-	sc->name = (char*) malloc(20*sizeof(char));
-
-	unsigned char i = 0, character_read;
-	while((character_read = fgetc(file)) != EOF && character_read != '\n' && i < 20){
-		sc->name[i] = character_read;
-		i++;
-	}
-
-	if(i >= 20){
-		while((character_read = fgetc(file)) != EOF && character_read != '\n');
-	} else if(character_read == EOF)
+	if(feof(file))
 		return NULL;
 
-	fscanf(file, "%d", sc->points);
+	char buf;
+
+	score* sc = (score*) malloc(sizeof(score));
+	sc->name = (char*) malloc(20*sizeof(char));
+	unsigned char length;
+
+	fgets(sc->name, 20, file);
+	length = strlen(sc->name);
+
+	sc->name[length-1] = '\0';
+	length--;
+
+	sc->name = (char*) realloc(sc->name, length*sizeof(char));
+
+	fscanf(file, "%u\n", &(sc->points));
 
 	return sc;
 }
 
-int highscore_destruct(){
+int highscore_add(char* name, unsigned int points){
+	score* sc = (score*) malloc(sizeof(score));
+	sc->name = name;
+	sc->points = points;
+
 	unsigned char i;
 	for(i = 0; i < highscore_size; i++){
-		printf("Name: %s\tScore: %d", highscores[i]->name, highscores[i]->points);
+		if(points > highscores[i]->points)
+			break;
+	}
+
+	highscore_size++;
+	highscores = (score**) realloc(highscores, highscore_size*sizeof(score*));
+
+	unsigned char j;
+	for(j = highscore_size; j > i; j--){
+		highscores[j] = highscores[j-1];
+	}
+	highscores[i] = sc;
+
+	return 0;
+}
+
+int highscore_write(FILE* file, score* sc){
+	fprintf(file, "\n%s\n%u", sc->name, sc->points);
+	return 0;
+}
+
+int highscore_destruct(){
+	FILE* file;
+	if((file = fopen(highscore_path, "w")) == NULL)
+		return 1;
+
+	printf("size: %d\n", highscore_size);
+
+	fprintf(file, "//MINIX INVADERS HIGHSCORES//");
+
+	unsigned char i;
+	for(i = 0; i < highscore_size && i < MAX_NUMBER_OF_SCORES; i++){
+		printf("Name: %s\tScore: %u\n", highscores[i]->name, highscores[i]->points);
+		highscore_write(file, highscores[i]);
 		free(highscores[i]);
 	}
+
 	free(highscores);
+	fclose(file);
+	highscore_size = 0;
 
 	return 0;
 }
--- proj/highscore.h
+++ proj/highscore.h
@@ -7,7 +7,7 @@
 
 typedef struct{
 	char* name;
-	int points;
+	unsigned int points;
 } score;
 
 static char highscore_path[] = "/home/lcom/lcom1516-t6g12/proj/highscore.txt";
@@ -17,6 +17,10 @@ static unsigned char highscore_size;
 int highscore_init();
 
 score* highscore_read(FILE* file);
+
+int highscore_add(char* name, unsigned int points);
+
+int highscore_write(FILE* file, score* sc);
 
 int highscore_destruct();
 
