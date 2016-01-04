0f5bc8a593f3ed2e081f10ddf2bbc95cd8a5da7a
up201404131 <up201404131@c565d994-3a90-4353-ac06-00fa9734bba2>
2016-01-04 05:14:33 +0000

    Documented the code relevant to the lab

--- lab7/uart.h
+++ lab7/uart.h
@@ -56,50 +56,213 @@ static fifo *com2_transmit_fifo;
 #define LSR_THR_EMPTY		BIT(5)
 
 
-
+/**
+ * @brief subscribes to the serial port interrupts
+ *
+ * @param base_addr COM port to subscribe (1 or 2)
+ * @ret 0 if successful
+ */
 int serial_subscribe_int(unsigned short base_addr);
+
+/**
+ * @brief unsubscribes serial port interrupts
+ *
+ * @param base_addr COM port to unsubscribe(1 or 2)
+ * @ret 0 if successful
+ */
 int serial_unsubscribe_int(unsigned short base_addr);
+
+/**
+ * @brief checks the origin of the serial port interrupsts and handles them appropriately
+ *
+ * @param base_addr relevant COM port
+ * @param tx 1 if the VM is the transmitter
+ * @param rx 1 if the VM is the receiver
+ *
+ * @ret 0 if sucessful
+ */
 int serial_int_handler(unsigned short base_addr, char tx, char rx);
 
 /* LCR funcs*/
+
+/**
+ * @brief passes the configuration of the LCR to a char
+ *
+ * @param base_addr relevant COM port
+ * @param *st char pointer to store the configuration
+ *
+ * @ret 0 if successful
+ */
 int serial_get_conf(unsigned short base_addr, char *st);
+
+/**
+ * @brief changes the configuration of a char
+ *
+ * @param base_addr relevant COM port
+ * @param conf desired configuration of the LCR
+ *
+ * @ret 0 if successful
+ */
 int serial_set_conf(unsigned short base_addr, unsigned long conf);
 
 
 /* LSR Funcs */
+/**
+ * @brief passes the configuration of the LSR to a char
+ *
+ * @param base_addr relevant COM port
+ * @param *st char pointer to store the configuration
+ *
+ * @ret 0 if successful
+ */
 int serial_get_lsr(unsigned short base_addr, char *st);
 
 /* IER funcs */
+
+/**
+ * @brief passes the configuration of the IER to a char
+ *
+ * @param base_addr relevant COM port
+ * @param *st char pointer to store the configuration
+ *
+ * @ret 0 if successful
+ */
 int serial_get_ier(unsigned short base_addr, char *st);
 
+/**
+ * @brief checks if the receiver interrupts are enabled
+ */
 int serial_check_rx(unsigned short base_addr);
+
+/**
+ * @brief toggles the receiver interrupts
+ */
 int serial_toggle_rx(unsigned short base_addr);
 
+/**
+ * @brief checks if the transmitter interrupts are enabled
+ */
 int serial_check_tx(unsigned short base_addr);
+
+/**
+ * @brief toggles the transmitter interrupts
+ */
 int serial_toggle_tx(unsigned short base_addr);
 
+/**
+ * @brief checks if the LSR interrupts are enabled
+ */
 int serial_check_lsr_ints(unsigned short base_addr);
+
+/**
+ * @brief toggles the LSR interrupts
+ */
 int serial_toggle_lsr_ints(unsigned short base_addr);
 
 /*IIR Funcs*/
+
+/**
+ * @brief passes the configuration of the IIR to a char
+ *
+ * @param base_addr relevant COM port
+ * @param *st char pointer to store the configuration
+ *
+ * @ret 0 if successful
+ */
 int serial_get_iir(unsigned short base_addr, char *st);
 
 /* DL Funcs*/
+
+/**
+ * @brief toggles the DLAB
+ */
 int serial_toggle_dlab(unsigned short base_addr);
+
+/**
+ * @checks the state of the DLAB
+ */
 int serial_check_dlab(unsigned short base_addr);
+
+/**
+ * @brief passes the DL LSB to a char
+ *
+ * @param base_addr relevant COM port
+ * @param *st char pointer to store the configuration
+ *
+ * @ret 0 if successful
+ */
 int serial_get_dll(unsigned short base_addr, char *st);
+
+/**
+ * @brief passes the DL MSB to a char
+ *
+ * @param base_addr relevant COM port
+ * @param *st char pointer to store the configuration
+ *
+ * @ret 0 if successful
+ */
 int serial_get_dlm(unsigned short base_addr, char *st);
+
+/**
+ * @brief sets a new LSB for the DL
+ *
+ * @param base_addr relevant COM port
+ * @param dll desired DL LSB
+ *
+ * @ret 0 if successful
+ */
 int serial_set_dll(unsigned short base_addr, char dll);
+
+/**
+ * @brief sets a new MSB for the DL
+ *
+ * @param base_addr relevant COM port
+ * @param dll desired DL MSB
+ *
+ * @ret 0 if successful
+ */
 int serial_set_dlm(unsigned short base_addr, char dlm);
 
+/**
+ * @brief passes the configuration of the IIR to a char
+ *
+ * @param base_addr relevant COM port
+ * @param *st char pointer to store the configuration
+ *
+ * @ret 0 if successful
+ */
 int serial_get_line_status(unsigned short base_addr, char *st);
 
 /* FIFO funcs */
+
+/**
+ * @brief enables the UART FIFO
+ */
 int serial_enable_fifo(unsigned short base_addr,unsigned char trigger_level);
+
+/**
+ * @brief puts a char in the UART transmitter FIFO from the app specific code FIFO
+ */
 int serial_put_in_fifo(unsigned short base_addr, char word);
+
+/**
+ * @brief puts a char in the app specific receiver FIFO from the UART receiver FIFO
+ */
 int serial_get_from_fifo(unsigned short base_addr, char *word);
+
+/**
+ * @brief passes the maximum possible amount of chars in the UART transmitter FIFO from the app specific code FIFO
+ */
 int serial_fill_transmit(unsigned short base_addr);
+
+/**
+ * @brief passes the maximum possible amount of chars in the app specific receiver FIFO from the UART receiver FIFO
+ */
 int serial_clear_receive(unsigned short base_addr);
+
+/**
+ * @brief prints the received chars
+ */
 void print_receiver_fifo(unsigned short base_addr);
 
 
