afea188147b89cd8b2af85d71b005edad5b00e79
up201404131 <up201404131@c565d994-3a90-4353-ac06-00fa9734bba2>
2016-01-04 10:15:52 +0000

    Documented start_menu.h

--- proj/src/start_menu.h
+++ proj/src/start_menu.h
@@ -7,20 +7,46 @@ extern font* space_invaders_font;
 menu* start_menu;
 bitmap* title;
 
+/**
+ * @brief initializes the start menu
+ */
 void start_menu_init();
 
+/**
+ * @brief draws the main menu on the screen
+ */
 void start_menu_draw();
 
+/**
+ * @brief goes to the singleplayer
+ */
 void start_menu_singleplayer_on_click();
 
+/**
+ * @brief goes to the multiplayer
+ */
 void start_menu_multiplayer_on_click();
 
+/**
+ * @brief goes to the highscore table
+ */
 void start_menu_highscore_on_click();
 
+/**
+ *@brief goes to the options menu
+ */
 void start_menu_options_on_click();
 
+
+/**
+ * @brief exits the game
+ */
 void start_menu_exit_on_click();
 
+
+/**
+ * @brief deletes the start menu
+ */
 void start_menu_destruct();
 
 #endif
